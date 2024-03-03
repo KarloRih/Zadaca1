5d534c83c4deae0c0da22954b8b3c7a97068ac70
KarloRih <161939331+KarloRih@users.noreply.github.com>
2024-03-03 15:23:51 +0100

    Add files via upload

---
+++ zadaca1.py
@@ -0,0 +1,8 @@
+secret = 5
+guess = int(input ("Guess the secret number from 1 to 5:"))
+if guess == secret:
+    print ("Congratulations, secret number is 5!")
+else:
+    print ("Sorry, false number.")
+
+
